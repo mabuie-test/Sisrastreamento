090e9c8969fdbe198d84e175e9b9cd959473956f
JorgeMabuie <jorgemaabuie@gmail.com>
2025-06-17 11:40:10 +0200

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  tenantId:     { type: mongoose.Schema.Types.ObjectId, ref: 'Tenant', required: true },
+  tenantId:     { type: mongoose.Schema.Types.ObjectId, ref: 'Tenant', default: null },
   username:     { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   role:         { type: String, enum: ['super-admin','admin','user'], default: 'user' }
